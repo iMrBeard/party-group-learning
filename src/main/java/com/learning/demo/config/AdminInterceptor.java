a8073359fbfc2dd811a4af16959d2463726d7f3e
OswaldZero <wyh1726036835@126.com>
2020-08-04 11:40:03 +0800

    跨域设置

--- src/main/java/com/learning/demo/config/AdminInterceptor.java
+++ src/main/java/com/learning/demo/config/AdminInterceptor.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpSession;
 public class AdminInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        fixCrossDomain(request,response);
         HttpSession httpSession = request.getSession();
         String Uri = request.getRequestURI();
         //页面放行
@@ -15,9 +16,15 @@ public class AdminInterceptor implements HandlerInterceptor {
             if (Uri.contains("/login") || (httpSession.getAttribute("userLoginInfo")!=null)) {
                 return true;
             } else {
-                response.sendRedirect("/admin/login");
+                response.sendRedirect("../login");
+                return false;
             }
         }
         return true;
+    }
+    public void fixCrossDomain(HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse){
+        httpServletResponse.setHeader("Access-Control-Allow-Origin","*");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "*");
     }
 }
---
+++ src/main/java/com/learning/demo/config/LoginConfig.java
@@ -0,0 +1,14 @@
+//package com.learning.demo.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//@Configuration
+//public class LoginConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        InterceptorRegistration interceptorRegistration=registry.addInterceptor(new AdminInterceptor());
+//        interceptorRegistration.addPathPatterns("/**");
+//    }
+//}
